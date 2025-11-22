7bf2c9f419563432daa5be5f45e1565a9b8c0a1e
Colin <colin@colinbowes.net>
2025-11-21 19:33:07 -0500

    Added more subnets to fix deployment issues

--- main.tf
+++ main.tf
@@ -27,10 +27,15 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
 resource "aws_subnet" "public" {
   count                   = 3
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
   tags = {
@@ -38,12 +43,18 @@ resource "aws_subnet" "public" {
   }
 }
 
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
 resource "aws_subnet" "private" {
+  count             = 3
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
-    Name = "private"
+    Name = "private-${count.index}"
   }
 }
 
@@ -61,37 +72,45 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public[*].id
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_eip" "nat" {
+  count                     = 3
   domain                    = "vpc"
-  associate_with_private_ip = "10.0.2.2"
+  associate_with_private_ip = "10.0.${count.index + 4}.2"
   depends_on                = [aws_internet_gateway.igw]
+  tags = {
+    Name = "nat-eip-${count.index}"
+  }
 }
 
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.private.id
+  count         = length(var.private_subnet_cidrs)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.private[count.index].id
   depends_on    = [aws_internet_gateway.igw]
   tags = {
-    Name = "nat"
+    Name = "nat-${count.index}"
   }
 }
 
 resource "aws_route_table" "private" {
+  count  = length(var.private_subnet_cidrs)
   vpc_id = aws_vpc.main.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
   }
-  tags = { Name = "private-rt" }
+  tags = { Name = "private-rt-${count.index}" }
 }
 
 resource "aws_route_table_association" "private_assoc" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
 
 data "aws_ami" "ubuntu" {
@@ -110,6 +129,7 @@ resource "aws_launch_template" "app_template" {
   vpc_security_group_ids = [aws_security_group.instances.id]
   network_interfaces {
     associate_public_ip_address = true
+    security_groups             = [aws_security_group.instances.id]
   }
   monitoring {
     enabled = true
@@ -254,7 +274,7 @@ resource "aws_security_group" "instances" {
 
 resource "aws_db_subnet_group" "rds" {
   name       = "rds-subnet-group"
-  subnet_ids = [aws_subnet.private.id]
+  subnet_ids = aws_subnet.private[*].id
   tags = {
     Name = "rds-subnet-group"
   }
