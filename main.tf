f4463bde42f01423cd28e796a814af7db1b45d9f
Sumiran Jaiswal <sumi0309@umd.edu>
2025-12-02 20:41:44 -0500

    merge main.tf with s3.tf and cloufront.tf

--- main.tf
+++ main.tf
@@ -482,3 +482,102 @@ resource "aws_wafv2_web_acl_association" "main" {
   resource_arn = aws_lb.alb.arn
   web_acl_arn  = aws_wafv2_web_acl.main.arn
 }
+
+
+resource "random_id" "cdn_rand" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "cdn_bucket" {
+  bucket        = "enpm818n-cdn-bucket-${random_id.cdn_rand.hex}"
+  force_destroy = true
+
+  tags = {
+    Name = "enpm818n-cdn-bucket"
+  }
+}
+
+# Secure bucket (required to use with CloudFront)
+resource "aws_s3_bucket_public_access_block" "cdn_bucket_block" {
+  bucket                  = aws_s3_bucket.cdn_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "enpm818n-oac"
+  description                       = "OAC for S3 CDN bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled             = true
+  comment             = "enpm818n-cloudfront"
+  default_root_object = "index.html"
+
+  # ORIGIN → S3 BUCKET
+  origin {
+    domain_name              = aws_s3_bucket.cdn_bucket.bucket_regional_domain_name
+    origin_id                = "enpm818n-s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  # DEFAULT behavior: GET/HEAD + compression
+  default_cache_behavior {
+    target_origin_id       = "enpm818n-s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    # AWS managed CachingOptimized policy → includes GZIP/Brotli
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name = "enpm818n-cloudfront"
+  }
+}
+
+
+resource "aws_s3_bucket_policy" "cdn_bucket_policy" {
+  bucket = aws_s3_bucket.cdn_bucket.id
+  policy = data.aws_iam_policy_document.cdn_bucket.json
+}
+
+data "aws_iam_policy_document" "cdn_bucket" {
+  statement {
+    sid    = "AllowCloudFrontAccess"
+    effect = "Allow"
+
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.cdn_bucket.arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.cdn.arn]
+    }
+  }
+}
